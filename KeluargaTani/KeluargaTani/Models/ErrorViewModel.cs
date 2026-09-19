828938ceb9e294fd4837948a50c203cac2c4bb4f
Marshel Rinaldy <marshelrinaldy26@gmail.com>
2026-09-19 11:19:29 +0700

    Update Base Project

---
+++ KeluargaTani/KeluargaTani/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KeluargaTani.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
